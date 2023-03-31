489f46590d57859189c6adf7127b0c4875aea578
Santiago Sandoval <casaba3131@gmail.com>
2023-03-31 15:20:28 -0500

    Se modifican las rutas del aplicativo

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,25 +2,30 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductFormComponent } from './components/product-form/product-form.component';
+import { AuthenticationComponent } from './components/authentication/authentication.component'
 
 const routes: Routes = [
   
   {
-    path: '',
-    redirectTo: '/list-products',
+    path: 'home',
+    redirectTo: 'products',
     pathMatch: 'full'
   },
   {
-    path: 'list-products',
+    path: 'products',
     component: ProductListComponent
   },
   {
-    path: 'product-add',
+    path: 'product/add',
     component: ProductFormComponent
   },
   {
     path: 'product/edit/:id',
     component: ProductFormComponent
+  },
+  {
+    path: 'authentication',
+    component: AuthenticationComponent
   }
 ];
 
