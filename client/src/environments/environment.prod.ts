fe6c49b9312c590fad08bad8eb4d91d4ccd28fce
Mauriciotoro23191 <edwin_toro23191@elpoli.edu.co>
2023-11-30 23:06:32 -0500

    commit integracion

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,10 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api-jolie-jolie.onrender.com',
-<<<<<<< HEAD
   thisUrl:'https://jolie-jolie-admin.onrender.com',
   clientUrl: 'https://jolie-jolie-s6iq.onrender.com'
-=======
-  clientUrl:'http://localhost:49340'
->>>>>>> 65d735159992cf86a507d75c03dbe5e547ccd3f2
 };
